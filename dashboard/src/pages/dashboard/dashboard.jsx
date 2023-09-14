e7d7b8f2ef9aea1c51258ce440101e97e86600df
luismiguelalvarezmarin109 <luismiguelalvarezmarin109@gmail.com>
2023-09-14 16:21:00 -0500

    feat: :sparkles: card price

--- dashboard/src/pages/dashboard/dashboard.jsx
+++ dashboard/src/pages/dashboard/dashboard.jsx
@@ -1,74 +1,144 @@
 import './dashboard.css'
 import logo from './img/logo.png'
+import perfil from './img/perfil.jpg'
 import {FaBarsStaggered , FaBarsProgress} from 'react-icons/fa6'
-import {AiFillDashboard} from 'react-icons/ai'
-import {BsSearch, } from 'react-icons/bs'
+import {AiFillDashboard ,AiOutlineChrome , AiOutlineQuestionCircle , AiFillCheckCircle} from 'react-icons/ai'
+import {GoKey} from 'react-icons/go'
+import {TbTypography ,TbShadow ,TbBrandGooglePhotos} from 'react-icons/tb'
+import {IoColorPaletteOutline } from 'react-icons/io5'
+import {IoIosCash } from 'react-icons/io'
+import {BsSearch , BsNut , BsThreeDots} from 'react-icons/bs'
+import {HiOutlineBellAlert } from 'react-icons/hi2'
+import {GrUploadOption } from 'react-icons/gr'
+
+
 
 const dashboard = () => {
   return (
     <div className='general'>
-        <header className="header">
-            <div className="header__cabecera">
-                <div className="header__logo">
-                    <img src={logo} alt="" className="header__logo--img" />
-                    <h2 className='header__titulologo'>BERRY</h2>
+       <div className="navbar">
+            <div className="navbar__header">
+                <div className="navbar__logo">
+                    <img src={logo} alt="" className="navbar__img" />
+                    <h2 className="navbar__h2">BERRY</h2>
                 </div>     
-                <div className="header__bars">
-                    <FaBarsStaggered/>
-                </div>      
+                <div className="navbar__bars">
+                    <FaBarsStaggered className='navbar__bars--logo'/>
+                </div>
             </div>
-            <div className="header__body">
-                <div className=" header__items header__items--dashboard ">
-                    <h2 className="header__h2">Dashboard</h2>
-                    <button className="header__button">
-                        <div className="header__iconItems">
-                            <AiFillDashboard/>
+            <div className="vertical">
+                <div className="vertical__seccion">
+                    <span className="navbar__span">Dashboard</span>
+                    <a href='' className="vertical__layout">
+                        <AiFillDashboard/>
+                        <span className='vertical__span'>Dashboard</span>
+                    </a>
+                </div>
+                <div className="vertical__seccion">
+                    <span className="navbar__span">Pages</span>
+                    <span className='navbar__span navbar__span--small'>Pages Caption</span>
+                    <a href='' className="vertical__layout">
+                        <GoKey/>
+                        <span className='vertical__span'>Authentication</span>
+                    </a>
+                </div>
+                <div className="vertical__seccion">
+                    <span className="navbar__span">Utilities</span>
+                    <a href='' className="vertical__layout">
+                        <TbTypography/>
+                        <span className='vertical__span'>Typography</span>
+                    </a>
+                    <a href='' className="vertical__layout">
+                        <IoColorPaletteOutline/>
+                        <span className='vertical__span'>Color</span>
+                    </a>
+                    <a href='' className="vertical__layout">
+                        <TbShadow/>
+                        <span className='vertical__span'>Shadow</span>
+                    </a>
+                    <a href='' className="vertical__layout">
+                        <TbBrandGooglePhotos/>
+                        <span className='vertical__span'>Icons</span>
+                    </a>
+                </div>
+
+                <div className="vertical__seccion">
+                    <a href='' className="vertical__layout">
+                        <AiOutlineChrome/>
+                        <span className='vertical__span'>Sample Page</span>
+                    </a>
+                    <a href='' className="vertical__layout">
+                        <AiOutlineQuestionCircle/>
+                        <span className='vertical__span'>Documentation</span>
+                    </a>
+                </div>
+            </div>
+       </div>
+       <main className="main">
+            <div className="search">
+                    <form action="" className="search__form">
+                        <BsSearch className='search__icon'/>
+                        <input type="text" className="search__input" placeholder='Search' />
+                        <div className="search__bars">
+                            <FaBarsProgress className='search__faBars'/>
                         </div>
-                        <span>Dashboard</span>
-                    </button>
-                </div>
-                <div className=" header__items header__items--Pages">
-                    <h2 className="header__h2">Pages</h2>
-                    <span className='header__span--pages'>Page Caption</span>
-                    <button className="header__button">
-                        <span>Authentication</span>
-                    </button>
-                </div>
-                <div className=" header__items header__items--utilities">
-                    <h2 className="header__h2">Utilities</h2>
-                    <button className="header__button">
-                        <span>Typography</span>
-                    </button>
-                    <button className="header__button">
-                        <span>Color</span>
-                    </button>
-                    <button className="header__button">
-                        <span>Shadow</span>
-                    </button>
-                    <button className="header__button">
-                        <span>icons</span>
-                    </button>
-                </div>
-                <div className="header__items header__items--config">
-                    <button className="header__button">
-                        <span>Sample Page</span>
-                    </button>
-                    <button className="header__button">
-                        <span>Documentation</span>
-                    </button>
-                </div>
-            </div>       
-        </header>
-        <main className="main">
-            <div className="barra__superior">
-                <form className='barra__form' action="">
-                     <BsSearch/>
-                    <input className='barra__input' type="text" placeholder='search'/>
-                    <FaBarsProgress/>
-                </form>
+                        
+                    </form>
+                    <div className="user">
+                        <div className="user__config">
+                            <div className="user__noti">
+                                <HiOutlineBellAlert/>
+                            </div>
+                            <div className="user__element">
+                                <img src={perfil} alt="" className="user__img" />
+                                <BsNut/>
+                            </div>    
+                        </div> 
+                    </div>
             </div>
-        </main>
+
+            <div className="central">
+                <div className="central__header">
+                    <div className="central__card central__card--purple">
+                        <div className="central__buttonOptions">
+                            <BsThreeDots/>
+                        </div>
+                        <div className="central__button">
+                            <IoIosCash/>
+                        </div>
+                        <div className="central__price">
+                            <div className="central__valores">
+                                <h2 className="central__h2">$5000.00</h2>
+                                <div className="central__buttonUp">
+                                    <AiFillCheckCircle />
+                                </div> 
+                            </div>
+                            <span className="central__span">Total Earning</span>
+                        </div>
+                    </div>
+                    <div className="central__card central__card--blue">
+                        <div className="central__buttonOptions">
+                            <BsThreeDots/>
+                        </div>
+                        <div className="central__button">
+                            <IoIosCash/>
+                        </div>
+                        <div className="central__price">
+                            <div className="central__valores">
+                                <h2 className="central__h2">$5000.00</h2>
+                                <div className="central__buttonUp">
+                                    <AiFillCheckCircle />
+                                </div> 
+                            </div>
+                            <span className="central__span">Total Earning</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+       </main>
        
+
     </div>
   )
 }
